ba965c6db3a7d5abd98ed922059940484c51bdf3
Mark Williams <markzawill@gmail.com>
2016-05-23 13:52:33 -0500

    added more changes

---
+++ src/artgallery/Models/ArtShow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtGallery.Models
+{
+    public class ArtShow
+    {
+        public int ArtShowId { get; set; }
+        public int ArtWorkId { get; set; }
+        public int AgentId { get; set; }
+        public string ShowLocation { get; set; }
+        public float Overhead { get; set; }
+    }
+}
